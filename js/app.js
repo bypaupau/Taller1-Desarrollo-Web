454e6c348b8e6a8f4626bb02062aef9b949e76e4
pau <145233797+bypaupau@users.noreply.github.com>
2026-09-08 19:35:24 -0500

    feat: agregar estilos Bootstrap

--- js/app.js
+++ js/app.js
@@ -4,3 +4,47 @@ menu.forEach(plato=>{
     `<div class="menu-item">${plato.nombre}
     - $${plato.precio}</div>`;
 });
+
+const inputNombre = document.getElementById("nombre");
+const selectPlato = document.getElementById("plato");
+const inputCantidad = document.getElementById("cantidad");
+
+const btnAgregar = document.getElementById("btn-agregar");
+const tablaPedidos = document.getElementById("tabla-pedidos")
+
+// para agregar los platos
+menu.forEach(plato => {
+    const opcion = document.createElement("option");
+    opcion.value = plato.nombre;
+    opcion.textContent = `${plato.nombre} - $${plato.precio}`;
+    selectPlato.appendChild(opcion);
+});
+
+function agregarPedido(){
+    const nombre = inputNombre.value;
+    const plato = selectPlato.value;
+    const cantidad = inputCantidad.value;
+
+    if (nombre === ""){
+        alert("Ingresa el nombre del cliente");
+        return;
+    }
+
+    const fila = document.createElement("tr");
+    fila.innerHTML =
+    `
+    <td>${nombre}</td>
+    <td>${plato}</td>
+    <td>${cantidad}</td>
+    `;
+
+    tablaPedidos.appendChild(fila);
+
+    // Limpiar los pedidos
+    inputNombre.value = "";
+    selectPlato.value = "";
+    inputCantidad.value = "1";
+    inputNombre.focus();
+}
+
+btnAgregar.addEventListener("click", agregarPedido);
--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 const menu =[
     {nombre: "Ceviche", precio:5.50},
     {nombre: "Lomo saltado", precio:7.95},
-    {nombre: "Bolón de queso", precio:4.50}
+    {nombre: "Bolón de queso", precio:4.50},
+    {nombre: "Moro Chicloso", precio: 6.50}
 ]
